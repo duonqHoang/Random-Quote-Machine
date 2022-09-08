94594065d05310a0f386ed16155eef93c09485ab
duonqHoang <79439580+duonqHoang@users.noreply.github.com>
2022-09-08 17:21:25 +0700

    fix importing bug on Firefox

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import * as data from "./quotes.json" assert { type: "json" };
-
 const container = document.querySelector(".container");
 const quoteBox = container.querySelector(".quote-box");
 const quote = quoteBox.querySelector(".quote");
@@ -7,22 +5,21 @@ const author = quoteBox.querySelector(".author");
 const newQuoteButton = container.querySelector(".new-quote");
 const tweetButton = container.querySelector(".twitter-share-button");
 
-//get quotes from json file
-const quotes = data.default.quotes;
+let quotesArr;
 
-function getRandomQuote() {
+const getRandomQuote = function (quotes) {
   let randomNumber = Math.floor(Math.random() * quotes.length);
   return quotes[randomNumber];
-}
+};
 
-function changeQuote() {
-  let newQuote = getRandomQuote();
+const changeQuote = function (quotes) {
+  let newQuote = getRandomQuote(quotes);
   quote.textContent = newQuote.quote;
   author.textContent = "- " + newQuote.author;
   tweetButton.href =
     "https://twitter.com/intent/tweet?text=" +
     encodeURIComponent('"' + newQuote.quote + '"' + " - " + newQuote.author);
-}
+};
 
 function getRandomColor() {
   let randomColor = Math.floor(Math.random() * 16777215).toString(16);
@@ -39,13 +36,18 @@ function animateQuoteBox() {
 function buttonClick() {
   animateQuoteBox();
   setTimeout(() => {
-    changeQuote();
+    changeQuote(quotesArr);
   }, 500);
   container.style.backgroundColor = getRandomColor();
 }
 
 newQuoteButton.addEventListener("click", buttonClick);
 
-//Initial random quote and color
-changeQuote();
-container.style.backgroundColor = getRandomColor();
+fetch("./quotes.json")
+  .then((response) => response.json())
+  .then((quotes) => {
+    //Initial random quote and color
+    quotesArr = quotes.quotes;
+    changeQuote(quotesArr);
+    container.style.backgroundColor = getRandomColor();
+  });
